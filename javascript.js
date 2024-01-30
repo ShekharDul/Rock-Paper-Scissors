ee62822350c1d0b9f0aa8c4bb98b066e7148a338
Shekhar_Dulgach <shekhardulgach19@gmail.com>
2024-01-30 15:58:03 +0530

    Add logic to update score and announce winner once the total points equals 5

--- javascript.js
+++ javascript.js
@@ -1,56 +1,81 @@
-function getComputerChoice() {
+// The function below returns the computer's choice by using a random number generator
+function computerChoice () {
   let options = ['rock', 'paper', 'scissors'];
-  let choice = Math.floor((Math.random() * 2)+1);
-  return options[choice];
+  let randomNumber = Math.floor((Math.random() * 3));
+  let computerChoice = options[randomNumber];
+  return computerChoice;
 }
 
 
+// The code below will be executed when a button is clicked and will call the game function to start a round.
+function playRound() {
+  let playerSelection = (this.textContent || this.innerText).toLowerCase();
+  console.log(playerSelection);
+  let computerSelection = computerChoice();
+  console.log(computerSelection);
+  let result = game(playerSelection, computerSelection);
+  if (result == "It is a tie") {
+    userPoints = userPoints + 0;
+    computerPoints = computerPoints + 0;
+  } else if ( result == "You win") {
+    userPoints ++;
+  } else {
+    computerPoints ++;
+  }
+  updateScore();
 
-function game(){
-  let playerSelection = prompt("Type Rock, paper or scissors?").toLowerCase();
-
-  let computerSelection = getComputerChoice();
-
-
-  function playGround(computerSelection, playerSelection) {
-    console.log("The computer chose " +computerSelection);
-    console.log("You chose " + playerSelection);
-
-    if ( playerSelection === computerSelection) {
-      console.log("It is a tie!");
-    } else if (
-      (playerSelection === "rock" && computerSelection === "scissors") ||
-      (playerSelection === "paper" && computerSelection === "rock") ||
-      (playerSelection === "scissors" && computerSelection === "paper")) {
-        console.log("You win")
-        let result = "You win"
-        return result
+  if (userPoints+computerPoints == 5){
+    let finalResult = document.getElementById('final-results');
+    if(userPoints > computerPoints){
+      finalResult.innerText = "You won the previous game :) ";
+      updateScore(userPoints = 0, computerPoints = 0);
     } else {
-      console.log("You lose")
-      return "You lose";
+        finalResult.innerText = "You lost the previous game :(";
+        updateScore(userPoints = 0, computerPoints = 0);
     }
   }
-  return playGround(playerSelection, computerSelection);
 }
+
+function updateScore() {
+  let results = document.getElementById('results');
+  results.innerText = "User: " + userPoints + " & " + "Computer: " + computerPoints;
+}
+
+
+function game(playerSelection, computerSelection) {
+  if(playerSelection === computerSelection) {
+    return 'It is a tie';
+  }
+  if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
+   (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === "scissors" && computerSelection === "paper") ){
+    return "You win";
+  } else {
+    return "You lose";
+  };
+    
+};
+
+
 
 let userPoints = 0;
 let computerPoints = 0;
 
-for ( i = 0 ; i < 5; i++) {
-  let result = game();
-  if (result === "You win"){
-    userPoints++;
-  } else {
-    computerPoints++;
-  }
+// The code below will invoke functions when either of the buttons is clicked.
+
+for (let i = 0; i < 5; i++) {
+   
+
+    let rockButton = document.getElementById("rockButton");
+    rockButton.addEventListener('click', playRound);
+
+    let paperButton = document.getElementById("paperButton");
+    paperButton.addEventListener('click', playRound);
+
+    let scissorsButton = document.getElementById("scissorsButton");
+    scissorsButton.addEventListener('click', playRound);
+
+  
 }
 
-console.log("The user scored a total of " + userPoints + " points");
-console.log("The computer scored a total of  " + computerPoints + " points");
-
-if (userPoints>computerPoints) {
-  console.log("You win the game!")
-} else {
-  console.log("Sorry you lost the game");
-}
 
